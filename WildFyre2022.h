2925a202f58dc240628b599120e1707da69dcaee
Derek C <dcorreia@gmail.com>
2022-10-02 16:45:39 -0400

    define based on pinwiki switch, sol, and lamps move trident code to /trident folder for reference

---
+++ WildFyre2022.h
@@ -0,0 +1,283 @@
+#define LAMP_PLAYER_1_UP                    0
+#define LAMP_PLAYER_2_UP                    1
+#define LAMP_PLAYER_3_UP                    2
+#define LAMP_PLAYER_4_UP                    3
+#define LAMP_10K_ROLLOVER                   4
+#define LAMP_SAUCER_ARROW_1                 5
+#define LAMP_SAUCER_ARROW_2                 6
+#define LAMP_SAUCER_ARROW_3                 7
+#define LAMP_BALL_IN_PLAY                   8
+#define LAMP_BONUS_2K                       9
+#define LAMP_BONUS_4K                       10
+#define LAMP_BONUS_6K                       11
+#define LAMP_BONUS_8K                       12
+#define LAMP_BONUS_10K                      13
+#define LAMP_BONUS_20K                      14
+#define LAMP_5X_BONUS                       15
+#define LAMP_2X_BONUS                       16
+#define LAMP_CENTER_SAUCER_SPECIAL          17
+#define LAMP_TOP_EJECT_1                    18
+#define LAMP_TOP_EJECT_2                    19
+#define LAMP_TOP_EJECT_3                    20
+#define LAMP_EXTRA_BALL                     21
+#define LAMP_FYRE_F                         22
+#define LAMP_FYRE_Y                         23
+#define LAMP_FYRE_R                         24
+#define LAMP_FYRE_E                         25
+#define LAMP_GAME_OVER                      26
+#define LAMP_HIGHEST_SCORE                  27
+#define LAMP_LEFT_RETURN_SPECIAL            28
+#define LAMP_SPINNER                        29
+#define LAMP_MATCH                          30
+#define LAMP_PLAYER_1                       31
+#define LAMP_PLAYER_2                       32
+#define LAMP_PLAYER_3                       33
+#define LAMP_PLAYER_4                       34
+#define LAMP_4K_3BANK                       35
+#define LAMP_4K_EJECT                       36
+#define LAMP_2X_BONUS_3BANK                 37
+#define LAMP_SHOOT_AGAIN                    38  
+#define LAMP_TILT                           39
+#define LAMP_WILD_W                         40
+#define LAMP_WILD_I                         41
+#define LAMP_WILD_L                         42
+#define LAMP_WILD_D                         43
+
+// #define LAMP_PLAYFIELD_SPELLOUT_S         0
+// #define LAMP_PLAYFIELD_SPELLOUT_I1        1
+// #define LAMP_PLAYFIELD_SPELLOUT_L1        2
+// #define LAMP_PLAYFIELD_SPELLOUT_V         3
+// #define LAMP_PLAYFIELD_SPELLOUT_E         4
+// #define LAMP_PLAYFIELD_SPELLOUT_R         5
+// #define LAMP_PLAYFIELD_SPELLOUT_B         6
+// #define LAMP_PLAYFIELD_SPELLOUT_A1        7
+// #define LAMP_PLAYFIELD_SPELLOUT_L2        8
+// #define LAMP_PLAYFIELD_SPELLOUT_L3        9
+// #define LAMP_PLAYFIELD_SPELLOUT_M         10
+// #define LAMP_PLAYFIELD_SPELLOUT_A2        11
+// #define LAMP_PLAYFIELD_SPELLOUT_N         12
+// #define LAMP_PLAYFIELD_SPELLOUT_I2        13
+// #define LAMP_PLAYFIELD_SPELLOUT_A3        14
+// #define LAMP_TOPLANE_OUTER_WHEN_LIT       15
+// #define LAMP_PLAYFIELD_STANDUP_S          16
+// #define LAMP_PLAYFIELD_STANDUP_I1         17
+// #define LAMP_PLAYFIELD_STANDUP_L1         18
+// #define LAMP_PLAYFIELD_STANDUP_V          19
+// #define LAMP_PLAYFIELD_STANDUP_E          20
+// #define LAMP_PLAYFIELD_STANDUP_R          21
+// #define LAMP_PLAYFIELD_STANDUP_B          22
+// #define LAMP_PLAYFIELD_STANDUP_A1         23
+// #define LAMP_PLAYFIELD_STANDUP_L2         24
+// #define LAMP_PLAYFIELD_STANDUP_L3         25
+// #define LAMP_PLAYFIELD_STANDUP_M          26
+// #define LAMP_PLAYFIELD_STANDUP_A2         27
+// #define LAMP_PLAYFIELD_STANDUP_N          28
+// #define LAMP_PLAYFIELD_STANDUP_I2         29
+// #define LAMP_PLAYFIELD_STANDUP_A3         30
+// #define LAMP_RIGHT_SPINNER_1000_WHEN_LIT  31
+// #define LAMP_TOPLANE_CENTER_WHEN_LIT      32
+// #define LAMP_KICKER_LAMPS                 33
+// #define LAMP_15K_BONUS                    34
+// #define LAMP_30K_BONUS                    35
+// #define LAMP_BONUS_2X                     36
+// #define LAMP_BONUS_3X                     37
+// #define LAMP_BONUS_4X                     38
+// #define LAMP_KICKER_SPECIAL               39
+// #define LAMP_BONUS_5X                     40
+// #define LAMP_HEAD_MATCH                   41
+// #define LAMP_SHOOT_AGAIN                  42
+// #define LAMP_APRON_CREDIT                 43
+// #define LAMP_HEAD_L2                      44
+// #define LAMP_HEAD_L3                      45
+// #define LAMP_EXTRA_BALL                   46
+// #define LAMP_LEFT_SPINNER_1000_WHEN_LIT   47
+// #define LAMP_HEAD_BALL_IN_PLAY            48
+// #define LAMP_HEAD_HIGH_SCORE              49
+// #define LAMP_HEAD_GAME_OVER               50
+// #define LAMP_HEAD_TILT                    51
+// #define LAMP_HEAD_S                       52
+// #define LAMP_HEAD_I1                      53
+// #define LAMP_HEAD_L1                      54
+// #define LAMP_HEAD_V                       55
+// #define LAMP_HEAD_E                       56
+// #define LAMP_HEAD_R                       57
+// #define LAMP_HEAD_B                       58
+// #define LAMP_HEAD_A                       59
+
+
+// #define SW_KICKER_ROLLOVER        0
+// #define SW_HOOP_ROLLOVER_BUTTON   1
+// #define SW_TOP_RIGHT_ROLLOVER     2
+// #define SW_TOP_CENTER_ROLLOVER    3
+// #define SW_TOP_LEFT_ROLLOVER      4
+// #define SW_CREDIT_RESET           5
+// #define SW_TILT                   6
+// #define SW_OUTHOLE                7
+// #define SW_COIN_3                 8
+// #define SW_COIN_2                 9
+// #define SW_COIN_1                 10
+// #define SW_SLAM                   15
+// #define SW_RIGHT_A_TARGET         16
+// #define SW_LEFT_B_TARGET          17
+// #define SW_LEFT_R_TARGET          18
+// #define SW_LEFT_E_TARGET          19
+// #define SW_TOP_V_TARGET           20
+// #define SW_TOP_L_TARGET           21
+// #define SW_TOP_I_TARGET           22
+// #define SW_TOP_S_TARGET           23
+// #define SW_RIGHT_SPINNER          24
+// #define SW_RIGHT_OUTLANE_A        25
+// #define SW_RIGHT_INLANE_I         26
+// #define SW_CENTER_TARGET          27
+// #define SW_LEFT_INLANE_A          28
+// #define SW_LEFT_OUTLANE_M         29
+// #define SW_LOWER_L_SIDE_TARGET    30
+// #define SW_UPPER_L_SIDE_TARGET    31
+// #define SW_LEFT_SPINNER           32
+// #define SW_REBOUNDS_AND_TOP       33
+// #define SW_BACKGLASS_SILVERBALL_SW  34
+// #define SW_RIGHT_SLINGSHOT        35
+// #define SW_LEFT_SLINGSHOT         36
+// #define SW_CENTER_BUMPER          37
+// #define SW_RIGHT_BUMPER           38
+// #define SW_LEFT_BUMPER            39
+
+// Defines for switches
+#define SW_CREDIT_RESET   5
+#define SW_TILT           6
+#define SW_OUTHOLE        7
+#define SW_COIN_1         9
+#define SW_COIN_2         10
+#define SW_COIN_3         8
+#define SW_SLAM           15
+
+#define SW_RIGHT_OUTLANE  0
+#define SW_RIGHT_RETURN   1
+#define SW_LEFT_OUTLANE   2
+#define SW_LEFT_RETURN    3
+#define SW_RIGHT_MID_LANE 12
+#define SW_LEFT_MID_LANE  26
+
+#define SW_EB_ROLLOVER    31
+
+#define SW_ADVANCE_ARROW    5
+#define SW_ADVANCE_TARGET   17
+
+#define SW_ROLLOVER_10PT  16
+#define SW_10PT           19
+
+#define SW_EJECT_3        21
+#define SW_EJECT_2        22
+#define SW_EJECT_1        23
+#define SW_EJECT_BONUS    39
+
+#define SW_4DROP_1        11
+#define SW_4DROP_2        19
+#define SW_4DROP_3        27
+#define SW_4DROP_4        35
+#define SW_3DROP_1        20
+#define SW_3DROP_2        28
+#define SW_3DROP_3        36
+
+#define SW_CENTER_BUMPER    32
+#define SW_RIGHT_BUMPER     33
+#define SW_LEFT_BUMPER      34
+#define SW_RIGHT_SLINGSHOT  37
+#define SW_LEFT_SLINGSHOT   38
+
+#define SW_SPINNER        14
+
+
+
+
+
+#define SOL_KNOCKER       16
+#define SOL_OUTHOLE       9
+#define SOL_RIGHT_BUMPER  7
+#define SOL_LEFT_BUMPER   6
+#define SOL_CENTER_BUMPER 8
+#define SOL_LEFT_SLINGSHOT   15
+#define SOL_RIGHT_SLINGSHOT    10
+#define SOL_SOUND_10      0
+#define SOL_SOUND_100     1
+#define SOL_SOUND_1K      2
+#define SOL_SOUND_10K     3
+
+#define SOL_EJECT_BONUS     11
+#define SOL_EJECT_TOP       13
+
+#define SOL_3BANK_RESET     12
+#define SOL_4BANK_RESET     14
+
+
+//#define SOL_NONE          15
+
+#define SOLCONT_FLIPPERS      0x80
+#define SOLCONT_COIN_LOCKOUT  0x01
+
+#define NUM_SWITCHES_WITH_TRIGGERS          5
+#define NUM_PRIORITY_SWITCHES_WITH_TRIGGERS 5
+
+struct PlayfieldAndCabinetSwitch SolenoidAssociatedSwitches[] = {
+  { SW_RIGHT_SLINGSHOT, SOL_RIGHT_SLINGSHOT, 4},
+  { SW_LEFT_SLINGSHOT, SOL_LEFT_SLINGSHOT, 4},
+  { SW_LEFT_BUMPER, SOL_LEFT_BUMPER, 3},
+  { SW_CENTER_BUMPER, SOL_CENTER_BUMPER, 3},
+  { SW_RIGHT_BUMPER, SOL_RIGHT_BUMPER, 3}
+};
+
+
+// #define LAMP_ANIMATION_STEPS  16
+// byte LampAnimations[3][LAMP_ANIMATION_STEPS][8] = {
+//   // Radial
+//   {{0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00},
+// {0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00},
+// {0x00, 0x00, 0x01, 0x10, 0x00, 0x00, 0x00, 0x00},
+// {0x00, 0x55, 0x01, 0x10, 0x00, 0x81, 0x00, 0x00},
+// {0x00, 0xAA, 0x02, 0x10, 0x00, 0x41, 0x00, 0x00},
+// {0x00, 0x80, 0x02, 0x10, 0x00, 0x00, 0x00, 0x00},
+// {0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00},
+// {0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00},
+// {0x01, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00},
+// {0x00, 0x00, 0x00, 0x10, 0x00, 0x08, 0x00, 0x00},
+// {0x00, 0x00, 0x00, 0x10, 0x10, 0x38, 0x00, 0x00},
+// {0x08, 0x00, 0x00, 0x10, 0xA0, 0x04, 0x00, 0x00},
+// {0x12, 0x00, 0x00, 0x10, 0xC0, 0x04, 0x00, 0x00},
+// {0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00},
+// {0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00},
+// {0x04, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00}},
+// // Center out
+//   {{0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00},
+// {0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00},
+// {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+// {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+// {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+// {0x00, 0x00, 0x03, 0x00, 0x80, 0x00, 0x00, 0x00},
+// {0x00, 0x80, 0x03, 0x00, 0x90, 0x00, 0x00, 0x00},
+// {0x05, 0xC0, 0x01, 0x00, 0x30, 0x00, 0x00, 0x00},
+// {0x05, 0x70, 0x00, 0x00, 0x60, 0x08, 0x00, 0x00},
+// {0x00, 0x3C, 0x00, 0x00, 0x40, 0x08, 0x00, 0x00},
+// {0x00, 0x07, 0x00, 0x00, 0x00, 0x04, 0x00, 0x00},
+// {0x00, 0x01, 0x00, 0x00, 0x00, 0xE0, 0x00, 0x00},
+// {0x10, 0x00, 0x00, 0x00, 0x00, 0xE1, 0x00, 0x00},
+// {0x10, 0x00, 0x00, 0x00, 0x00, 0x11, 0x00, 0x00},
+// {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+// {0x0A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}},
+// // Bottom to top
+//   {{0x00, 0x00, 0x00, 0x00, 0x00, 0xC1, 0x00, 0x00},
+// {0x00, 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+// {0x00, 0x0E, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+// {0x00, 0x70, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+// {0x00, 0x80, 0x03, 0x00, 0x00, 0x00, 0x00, 0x00},
+// {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+// {0x00, 0x00, 0x00, 0x10, 0x00, 0x00, 0x00, 0x00},
+// {0x05, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+// {0x00, 0x00, 0x00, 0x00, 0x90, 0x00, 0x00, 0x00},
+// {0x00, 0x00, 0x00, 0x00, 0x60, 0x08, 0x00, 0x00},
+// {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+// {0x00, 0x00, 0x00, 0x00, 0x00, 0x24, 0x00, 0x00},
+// {0x00, 0x00, 0x00, 0x00, 0x00, 0x10, 0x00, 0x00},
+// {0x10, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+// {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00},
+// {0x0A, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00}}
+// };
